44a660e3aeb4bcafda71cce050baf7e4aebeb85c
khidilov <100746123+khidilov@users.noreply.github.com>
2022-06-09 21:51:05 -0700

    Add files via upload

---
+++ 06thofAprilHW/GT_Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using GT_Collections.MODELS;
+using System;
+
+namespace GT_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<int> a = new MyList<int>();
+            a.Additem(6);
+            Console.WriteLine("1.Addded");
+            Console.WriteLine("Length" + a.Length);
+            a.Additem(6);
+            Console.WriteLine("1.Addded");
+            Console.WriteLine("Length" + a.Length);
+            a.Additem(6);
+            Console.WriteLine("1.Addded");
+            Console.WriteLine("Length" + a.Length);
+            a.Additem(6);
+            Console.WriteLine("1.Addded");
+            Console.WriteLine("Length" + a.Length);
+            a.Additem(6);
+            Console.WriteLine("1.Addded");
+            Console.WriteLine("Length" + a.Length);
+            a.Additem(6);
+            Console.WriteLine("1.Addded");
+            Console.WriteLine("Length" + a.Length);
+        }
+
+    }
+}
---
+++ 06thofAprilHW/GT_Collections/models/Numbers.cs
@@ -0,0 +1,6 @@
+﻿namespace GT_Collections.MODELS
+{
+    public class Numbers
+    {
+    }
+}
